44873d6cf19de61b5fa141ae06f3ee81639bd573
Winder <wan_maple@hotmail.com>
2015-08-21 20:25:43 +0800

    add android crypto aes 256

--- frameworks/runtime-src/proj.android/src/org/cocos2dx/javascript/AndroidUtils.java
+++ frameworks/runtime-src/proj.android/src/org/cocos2dx/javascript/AndroidUtils.java
@@ -7,6 +7,20 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Debug;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 @SuppressLint("NewApi") 
 public class AndroidUtils {
 	public static void copyToPasteBoard(String content) {
@@ -32,5 +46,45 @@ public class AndroidUtils {
 			return 1;
 		}
 		return 2;
-	}
+    }
+    
+    public static byte[] encipherAes256(byte[] clearText, String keyString) throws NullPointerException {
+        if (keyString == null || keyString.length() == 0) {
+            throw new NullPointerException("Please give Password");
+        }
+        
+        if (clearText == null || clearText.length <= 0) {
+            throw new NullPointerException("Please give clearText");
+        }
+        
+        try {
+            SecretKeySpec skeySpec = getKey(keyString);
+            
+            // IMPORTANT TO GET SAME RESULTS ON iOS and ANDROID
+            final byte[] iv = new byte[16];
+            Arrays.fill(iv, (byte) 0x00);
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            
+            // Cipher is not thread safe
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+            
+            return cipher.doFinal(clearText);
+            
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+        return null;
 }
